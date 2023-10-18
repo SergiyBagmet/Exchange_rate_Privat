a2d58f3ceadb0b8183932cc81a67bcedaf497279
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-10-18 11:19:14 +0300

    a_worker_privat

--- ex_privat.py
+++ ex_privat.py
@@ -10,33 +10,29 @@ from my_lib.my_date import DateDeltaDay
 
 URL_PRIVATE_BODY = 'https://api.privatbank.ua/p24api/exchange_rates?json&date='
 
+def get_urls(delta_d: int) -> list[str]:
+    delta_5 = DateDeltaDay(date.today(), delta_d, negative=True)
+    return [URL_PRIVATE_BODY + date.strftime('%d.%m.%Y') for date in delta_5.get_min_max_date()]
 
 class AsyncApiRequester:
     
-    def __init__(self):
-        self.session = aiohttp.ClientSession()
+    def __init__(self, url):
+       self.url = url
 
-    async def get_json(self, url):
+    async def get_json(self):
         try:
-            async with self.session.get(url) as response:
-                if response.status == 200:
-                    data_json = await response.json()
-                    return data_json
-                else:
-                    print(f"Error status: {response.status} for {url}")
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url) as response:
+                    if response.status == 200:
+                        return await response.json()
+                    else:
+                        print(f"Error status: {response.status} for {self.url}")
         except aiohttp.ClientConnectorError as err:
-            print(f'Connection error: {url}', str(err))
+            print(f'Connection error: {self.url}', str(err))
     
-    async def close_session(self):
-        if self.session:
-            await self.session.close()
-    
-    def __del__(self):
-        if self.session and not self.session.closed:
-            self.session.close()
 
 
-async def a_parse_api_privat(
+async def a_parse_data_privat(
     data: dict[str, str | int | list[dict[str, str | float]]], 
     currencys: tuple[t.LiteralString]=("EUR", "USD")
     ):# TODO
@@ -50,25 +46,20 @@ async def a_parse_api_privat(
                                 }
             }
 
-async def a_fasade(url, a_requester: AsyncApiRequester):
-        data = await a_requester.get_json(url)
-        return await a_parse_api_privat(data)
 
-async def main(urls):
-    a_requester = AsyncApiRequester()
-    results = await asyncio.gather(*[a_fasade(url, a_requester) for url in urls], return_exceptions=True) 
-    await a_requester.close_session()
-    return results
+async def a_worker_privat(url):
+    requester = AsyncApiRequester(url)
+    data_json = await requester.get_json()
+    return await a_parse_data_privat(data_json)
 
-def get_urls(delta_d: int) -> list[str]:
-    delta_5 = DateDeltaDay(date.today(), delta_d, negative=True)
-    return [URL_PRIVATE_BODY + date.strftime('%d.%m.%Y') for date in delta_5.get_min_max_date()]
-       
+async def main_privat():
+    delta_d = int(sys.argv[1])
+    urls = get_urls(delta_d)  
+    return await asyncio.gather(*[a_worker_privat(url) for url in urls], return_exceptions=True) 
+     
 if __name__ == "__main__":
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     
-    delta_d = int(sys.argv[1])
-    urls = get_urls(delta_d)  
-    print(asyncio.run(main(urls)))
+    print(asyncio.run(main_privat()))
    
